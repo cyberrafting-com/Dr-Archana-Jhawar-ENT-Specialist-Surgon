337eec9538fa3ad9397d9a6081ccf32aa1bddc65
HemlataJadhav04 <164496711+HemlataJadhav04@users.noreply.github.com>
2024-06-25 12:55:04 +0530

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -98,7 +98,7 @@
             <div class="col-lg-9 col-md-8 col-12 ">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-12">
-                        <h2>Our Locations in mumbai</h2>
+                        <h2>Our Locations in mumbai pune delhi</h2>
                     </div>
                 </div>
                 <div class="row">
